676ac91cf8836066a47169e08c20f41c5c661bb0
takata150802 <takata@hal.ipc.i.u-tokyo.ac.jp>
2018-05-05 20:49:14 +0900

    fix setup.ubuntu.sh

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -50,10 +50,9 @@ echo 'export PYENV_ROOT=$HOME/.pyenv' >> ~/.bashrc
 echo 'export PATH=$PYENV_ROOT/bin:$PATH' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source ~/.bashrc
+wait
 echo  $PATH
-. ~/.bashrc
-echo  $PATH
-exit
+
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev
 sudo apt-get install -y libfreetype6-dev libblas-dev liblapack-dev gfortran tk-dev libhdf5-dev python-dev
